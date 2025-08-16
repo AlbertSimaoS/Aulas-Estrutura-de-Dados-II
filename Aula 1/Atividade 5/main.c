6c37803be354ec46cd2eb41d801cb38c200ba68f
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-08-15 23:37:00 -0300

    Create main.c

---
+++ Aula 1/Atividade 5/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab.h"
+
+int main()
+{
+    int vetor[] = {120, 150, 110, 130, 100, 160, 140, 190, 180, 170};
+    int i;
+
+    printf("Vetor desordenado: ");
+    for(i = 0; i < 10; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+
+    printf("\n\nOrdenando por insertionSort:");
+
+    Ordena_insertionSort(vetor, 10);
+
+    printf("\n\nVetor ja ordenado: ");
+    for(i = 0; i < 10; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+
+    printf("\n\n");
+
+    return 0;
+}
